dcc3fdbd7de6100af80b1765ad634521fedd78de
pure-hate <chuchundra06@gmail.com>
2022-05-14 19:15:20 +0300

    валидация ввода, кликабельные ссылки на телегу, верстка
    теперь имя ограничено 100 символами, текст - 300, и телеграмм 50 символами
    преобразование ссылки на телеграмм в кликабельную
    валидация координат
    небольшая верстка

--- create.php
+++ create.php
@@ -14,15 +14,34 @@
     $telegram = $con->real_escape_string($_POST["telegram"]);
     $text = $con->real_escape_string($_POST["text"]);
 	
+	$name = substr($name, 0, 100);
+	$telegram = substr($telegram, 0, 50);
+	$text = substr($text, 0, 300);
 	
-	    $sql = "insert into anonlist (lat, lon, name,telegram, text) values ('$lat', '$lon','$name','$telegram','$text')";
-    if($con->query($sql)){
-        echo "Маркер добавлен";
-		echo '<script type="text/javascript">';
-		echo 'window.location=document.referrer;';
-		echo '</script>';
-    } else{
-        echo "Ошибка: " . $con->error;
-    }
+	if ($telegram[0]!="@")
+	{
+		$telegram= "@".$telegram;
+	}
+	
+	if (is_numeric($lat) && is_numeric($lon)){
+	
+		$sql = "insert into anonlist (lat, lon, name,telegram, text) values ('$lat', '$lon','$name','$telegram','$text')";
+		
+		if($con->query($sql))
+		{
+			echo "Маркер добавлен";
+			echo '<script type="text/javascript">';
+			echo 'window.location=document.referrer;';
+			echo '</script>';
+		}
+		else
+		{
+			echo "Ошибка: " . $con->error;
+		}
+	}
+	else
+	{
+		echo "Ошибка: координаты попердолены";
+	}
     $con->close();
 ?>
--- index.php
+++ index.php
@@ -17,7 +17,11 @@
 				
 				map.on('click', function(e) {
                     document.getElementById('form').style.visibility = "visible";
+					document.getElementById('form').style.width='100%';
+					
 					document.getElementById('info').style.visibility = "hidden";
+					document.getElementById('info').style.width='1px';
+					
 					document.getElementById('lat').value = e.latlng.lat;
 					document.getElementById('lon').value = e.latlng.lng;
 					
@@ -30,18 +34,18 @@
 				function AddMarker(coords, clickName, clickTelegram, clickText) {
 								DG.marker(coords).bindPopup(clickName).on('click', function() {
 									document.getElementById('form').style.visibility = "hidden";
+									document.getElementById('form').style.width='1px';
+									
 									document.getElementById('info').style.visibility = "visible";
+									document.getElementById('info').style.width='100%';
+									
                           document.getElementById('named').innerText = clickName;
 						  document.getElementById('telegram').innerText = clickTelegram;
+						  document.getElementById('telegram').href = "https://telegram.im/"+clickTelegram;
 						  document.getElementById('text').innerText = clickText;
                     }).addTo(map);
 				}
-				
 
-
-				
-
-				
 				<?php
 				print "AddMarker([54.98, 82.89],'Anon 3', '@ololo', 'testtext');";
 				include 'database.php';
@@ -51,26 +55,34 @@
               
             });
         </script>
-        <div id="map" style="width:500px; height:400px"></div>
-		<div id='info'>
-	<span id="named"> </span><br>
-	<span id="telegram"> </span><br>
-	<span id="text"> </span>
-	</div>
-	<div id='form' style="visibility: hidden">
-	<p>Добавить свою метку:</p>
-	<form name="add_form" action="create.php" method="post">
-	<input type="text" id="lat" name="lat" value="lat" style="display:none"; />
-	<input type="text" id="lon" name="lon" value="lon" style="display:none"; />
-	<br>
-    <p>Имя:
-    <input type="text" name="name" /></p>
-    <p>Телеграмм
-    <input type="text" name="telegram" /></p>
-	<p>Текст
-	<input type="text" name="text" /></p>
-    <input type="submit" value="Добавить">
-</form>
-</div>
+		
+    <div id="map" style="width:100%; height:500px;"></div>
+	
+
+	
+		<div id='info' style="display:table-cell;width:50%; word-wrap: break-word;">
+			<span id="named"> </span><br>
+			<a id="telegram" href="" target="_blank"></a><br>
+			<span id="text" > </span>
+		</div>
+		
+		<div id='form' style="display:table-cell; visibility: hidden float:right;">
+			<p>Добавить свою метку:</p>
+			<form name="add_form" action="create.php" method="post">
+			<input type="text" id="lat" name="lat" value="lat" style="display:none"; size="0"; />
+			<input type="text" id="lon" name="lon" value="lon" style="display:none"; size="0"; />
+			<br>
+			Имя:
+			<input type="text" name="name" />
+			Телеграмм
+			<input type="text" name="telegram" />
+			Текст
+			<input type="text" name="text" />
+			<input type="submit" value="Добавить">
+		</form>
+		</div>
+	
+
+		
     </body>
 </html>
